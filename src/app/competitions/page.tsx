4cb6ee6473d94e471e39c4e0b3862fb4b8d306c0
TuJPan <joao.pf@estudante.iftm.edu.br>
2026-06-01 14:34:07 -0300

    Create page.tsx

---
+++ src/app/competitions/page.tsx
@@ -0,0 +1,9 @@
+export default function CompetitionsPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-4xl font-bold">
+        Competições
+      </h1>
+    </main>
+  );
+}
