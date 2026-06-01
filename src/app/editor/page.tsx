e81fdcccb288983b4eb051beba6d47d5d3d1ce47
TuJPan <joao.pf@estudante.iftm.edu.br>
2026-06-01 14:35:15 -0300

    Create page.tsx

---
+++ src/app/editor/page.tsx
@@ -0,0 +1,9 @@
+export default function EditorPage() {
+  return (
+    <main className="p-8">
+      <h1 className="text-4xl font-bold">
+        Editor de Código
+      </h1>
+    </main>
+  );
+}
